5a8ff59f3db7a59c39bf55994ad0dd6c0c8865b7
Frillion <fridriksoebech@gmail.com>
2025-01-28 20:18:53 +0000

    fixed a error from old conf

--- lua/Frillion/plugins/todo-comments.lua
+++ lua/Frillion/plugins/todo-comments.lua
@@ -4,10 +4,10 @@ return {
     config = function()
         local todo_comments = require("todo-comments")
 
-        vim.keymaps.set("n","[t", function()
+        vim.keymap.set("n","[t", function()
             todo_comments.jump_next()
         end,{ desc = "Jump next todo comment"})
-        vim.keymaps.set("n","]t", function()
+        vim.keymap.set("n","]t", function()
             todo_comments.jump_prev()
         end, { desc = "Jump prev todo comment." })
         todo_comments.setup()
